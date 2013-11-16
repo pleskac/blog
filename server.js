6fd4287a33694b3c37f4024dd1f87246698b4db0
pleskac <markpleskac@gmail.com>
2013-11-16 19:27:06 +0000

    sql package installed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-/*var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'pleskac.org',
+  user     : 'root',
+  password : 'rootroot',
+});
 
-*/
+connection.connect(function(err) {
+  // connected! (unless `err` is set)
 
+});
 
 var sys = require("sys"),  
 my_http = require("http");  
@@ -23,7 +25,7 @@ my_http.createServer(function(request,response){
     //sys.puts(full_path);  
     //select the files based on my_path
     response.writeHeader(200, {"Content-Type": "text/plain"});  
-    response.write("Hello World");  
+    response.write(postInt.toString());  
     response.end();  
 }).listen(1337);  
 sys.puts("Server Running on 1337");   
