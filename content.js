fb719afd4f1b24aead15cb73c1e8a8732663ea1c
pleskac <markpleskac@gmail.com>
2013-11-16 23:44:59 +0000

    prototype post works

--- content.js
+++ content.js
@@ -1,24 +1,30 @@
 $(document).ready(function () {
 	jQuery.ajax("http://pleskac.org:1337/5439").done(
                 function(data){
-			if (window.console) console.log(data);
 			image_urls = JSON.parse(data);
 			jQuery.each(image_urls, function(){
+				var imageURL = resizeToLarge(this.guid, this.meta_value);
+				
 				var imageString = '<div class="caption">';
-				imageString += '<img class="dynamic_image" src="' + this.guid + '" />';
-				imageString += '<span>' + this.post_excerpt + '</span>';
+				imageString += '<img class="dynamic_image" src="' + imageURL + '" />';
+				if(this.post_excerpt){
+					imageString += '<span>' + this.post_excerpt + '</span>';
+				}
 				imageString += '</div>';
-				//$('#post_content').append('<div class="caption">'); 
-				//$('#post_content').append('<img class="dynamic_image" src="' + this.guid + '" />');
-				//$('#post_content').append('<span>' + this.post_excerpt + '</span>');
-                        	if (window.console) console.log(imageString);
 				$('#post_content').append(imageString);
 			});
 	});
 });
 
-function resizeToLarge(uri){
-	uri = uri.replace('.jpg','');
-	uri.append('-1024x682.jpg');
+function resizeToLarge(uri, jibberish){
+	var pattern = new RegExp('((s:21:").+jpg")');
+	var match = jibberish.toString().match(pattern);
+	if(match != null){
+		var start = match.index + 6;
+		var end = match.index + match[0].length;
+		var imageName = (jibberish.toString().substring(start,end));
+		uri = uri.replace(/\/([^\/]*)$/,'/');
+		return uri + imageName;
+	}
 	return uri;
 }
