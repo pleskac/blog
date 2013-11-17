4534f7068c92396e65a6a4b925533d19e6736d97
Mark Pleskac <markpleskac@gmail.com>
2013-11-17 13:04:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'rootroot',
+  password : 'rootroot', //not a secret, lol
   database : 'wordpress',
 });
 
