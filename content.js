d9e903c23b404b9119a8246f2d45f827fa72e3d8
pleskac <markpleskac@gmail.com>
2013-11-24 17:21:42 +0000

    link color and order

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	if(pathname = "/blog/"){
 		jQuery.ajax("http://pleskac.org:1337/blog").done(
 	                function(data){
-				posts = JSON.parse(data);
+				posts = JSON.parse(data).reverse();
 				jQuery.each(posts, function(){
 					var link = '<a href="http://pleskac.org:1337/' + this.id + '" >' + this.post_title + '</a><br />';
 					$('#post_content').append(link);
