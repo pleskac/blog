27b89eb64d2c8ba3cdf846912861946db4f33494
Mark Pleskac <markpleskac@gmail.com>
2013-12-01 17:35:10 -0800

    fix?

--- server.go
+++ server.go
@@ -10,6 +10,7 @@ import (
 	_ "log"
 	"net/http"
 	"strings"
+	_ "unicode/utf8"
 )
 
 type Picture struct {
@@ -93,7 +94,7 @@ func getAllPosts() []Post {
 	for _, row := range rows {
 		title := row.Str(1)
 		if strings.Contains(row.Str(1), "\x96") {
-			strings.Replace(title, "\x96", "-", -1)
+			strings.Replace(title, "\x96", "\x20\x13", -1)
 			fmt.Println("New title:", title)
 		}
 		allPosts = append(allPosts, Post{row.Str(0), title})
