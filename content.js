d1ef9af40bacf14709317c8a59bc6b42a76adb6a
Mark Pleskac <markpleskac@gmail.com>
2013-12-02 18:39:48 -0800

    support golang server

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 	                function(data){
 				posts = JSON.parse(data).reverse();
 				jQuery.each(posts, function(){
-					var link = '<a href="http://pleskac.org:1337/' + this.id + '" >' + this.post_title + '</a><br />';
+					var link = '<a href="http://pleskac.org:1337/' + this.Id + '" >' + this.Post_title + '</a><br />';
 					$('#post_content').append(link);
 				});
 		});
