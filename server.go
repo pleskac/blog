e08cd625d790e173bb225c986949b41464251784
Mark Pleskac <markpleskac@gmail.com>
2013-12-02 18:28:21 -0800

    fix?

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//change to github.com/pleskac/SpotLocator/dblayer
+	"bytes"
 	"code.google.com/p/gorilla/mux"
 	"encoding/json"
 	"fmt"
@@ -9,7 +10,6 @@ import (
 	_ "github.com/ziutek/mymysql/native"
 	_ "log"
 	"net/http"
-	"strings"
 	_ "unicode/utf8"
 )
 
@@ -93,11 +93,8 @@ func getAllPosts() []Post {
 
 	for _, row := range rows {
 		title := row.Str(1)
-		if strings.Contains(row.Str(1), "\x96") {
-			title = strings.Replace(title, "\x96", "|", -1)
-			fmt.Println("New title:", title)
-		}
-		allPosts = append(allPosts, Post{row.Str(0), title})
+
+		allPosts = append(allPosts, Post{row.Str(0), fromWindows1252(title)})
 	}
 
 	return allPosts
@@ -128,4 +125,77 @@ func getPictures(p string) []Picture {
 	}
 
 	return pictures
+}
+
+//used from Stack Overflow
+//http://stackoverflow.com/questions/6927611/go-language-how-to-convert-ansi-text-to-utf8
+func fromWindows1252(str string) string {
+	var arr = []byte(str)
+	var buf = bytes.NewBuffer(make([]byte, 512))
+	var r rune
+
+	for _, b := range arr {
+		switch b {
+		case 0x80:
+			r = 0x20AC
+		case 0x82:
+			r = 0x201A
+		case 0x83:
+			r = 0x0192
+		case 0x84:
+			r = 0x201E
+		case 0x85:
+			r = 0x2026
+		case 0x86:
+			r = 0x2020
+		case 0x87:
+			r = 0x2021
+		case 0x88:
+			r = 0x02C6
+		case 0x89:
+			r = 0x2030
+		case 0x8A:
+			r = 0x0160
+		case 0x8B:
+			r = 0x2039
+		case 0x8C:
+			r = 0x0152
+		case 0x8E:
+			r = 0x017D
+		case 0x91:
+			r = 0x2018
+		case 0x92:
+			r = 0x2019
+		case 0x93:
+			r = 0x201C
+		case 0x94:
+			r = 0x201D
+		case 0x95:
+			r = 0x2022
+		case 0x96:
+			r = 0x2013
+		case 0x97:
+			r = 0x2014
+		case 0x98:
+			r = 0x02DC
+		case 0x99:
+			r = 0x2122
+		case 0x9A:
+			r = 0x0161
+		case 0x9B:
+			r = 0x203A
+		case 0x9C:
+			r = 0x0153
+		case 0x9E:
+			r = 0x017E
+		case 0x9F:
+			r = 0x0178
+		default:
+			r = rune(b)
+		}
+
+		buf.WriteRune(r)
+	}
+
+	return string(buf.Bytes())
 }
