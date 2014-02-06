b843cc0fcb06a0f98170b4407f6661d74404b7ef
Mark Pleskac <markpleskac@gmail.com>
2014-02-05 19:35:36 -0800

    different format

--- page.js
+++ page.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 		$('img.caption').captionjs({
         	'class_name' : 'captionjs',  // Class name assigned to each <figure>
         	'schema'     : true,         // Use schema.org markup (i.e., itemtype, itemprop)
-        	'mode'       : 'animated',    // default | static | animated | hide
+        	'mode'       : 'default',    // default | static | animated | hide
         	'debug_mode' : true         // Output debug info to the JS console
     	});
 	});
