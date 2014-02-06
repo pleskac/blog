32cc8f81170d7a9ec193b9ca9f7463643150d7c5
Mark Pleskac <markpleskac@gmail.com>
2014-02-05 19:32:05 -0800

    NOW I GOT IT

--- page.js
+++ page.js
@@ -1,10 +1,5 @@
 $(window).load(function() {
-    $('img.caption').captionjs({
-        'class_name' : 'captionjs',  // Class name assigned to each <figure>
-        'schema'     : true,         // Use schema.org markup (i.e., itemtype, itemprop)
-        'mode'       : 'animated',    // default | static | animated | hide
-        'debug_mode' : true         // Output debug info to the JS console
-    });
+
 });
 
 $(document).ready(function () {
@@ -25,6 +20,14 @@ $(document).ready(function () {
 
 			$('#post_content').append(imageString);
 		});
+
+		// we need to wait until the <img> are inserted
+		$('img.caption').captionjs({
+        	'class_name' : 'captionjs',  // Class name assigned to each <figure>
+        	'schema'     : true,         // Use schema.org markup (i.e., itemtype, itemprop)
+        	'mode'       : 'animated',    // default | static | animated | hide
+        	'debug_mode' : true         // Output debug info to the JS console
+    	});
 	});
 });
 
